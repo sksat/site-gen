1f6642bb1b8e5546d1b73903e6ba96edf7858b0f
sksat <sksat@sksat.net>
2021-03-31 01:01:03 +0900

    add: convert WriteDown

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use std::path::{Path, PathBuf};
 
 use clap::{App, Arg};
 use pulldown_cmark::{html, Options, Parser};
+use writedown::Render;
+use writedown_html;
 
 fn main() -> io::Result<()> {
     let matches = App::new(env!("CARGO_PKG_NAME"))
@@ -24,10 +26,6 @@ fn main() -> io::Result<()> {
         )
         .get_matches();
 
-    let mut options = Options::empty();
-    options.insert(Options::ENABLE_STRIKETHROUGH);
-    options.insert(Options::ENABLE_FOOTNOTES);
-
     let content = matches.value_of("CONTENT").unwrap();
     let content = Path::new(content);
 
@@ -35,40 +33,98 @@ fn main() -> io::Result<()> {
     let output = Path::new(output);
 
     let files = get_all_files(content)?;
-    let mds: Vec<&PathBuf> = files.iter().filter(|f| is_extension(f, "md")).collect();
+    for f in &files {
+        let ext = f.extension();
+        let path = f.strip_prefix(content).unwrap();
 
-    for file in &mds {
-        let path = file.strip_prefix(content).unwrap();
-
-        let mut file = io::BufReader::new(fs::File::open(file)?);
-        let mut md = String::new();
-        let _ = file.read_to_string(&mut md).unwrap();
-        let parser = Parser::new_ext(&md, options);
+        let mut file = io::BufReader::new(fs::File::open(f)?);
+        let mut src = String::new();
+        let _ = file.read_to_string(&mut src).unwrap();
 
         let path = output.join(path);
-        let path = path.with_extension("html");
         let parent = path.parent().unwrap();
         if parent.exists() {
             if !parent.is_dir() {
                 panic!("parent is not directory");
             }
-            println!(
-                "parent directory({}) is already exist",
-                parent.to_str().unwrap()
-            );
+            //println!(
+            //    "parent directory({}) is already exist",
+            //    parent.to_str().unwrap()
+            //);
         } else {
             fs::create_dir(path.parent().unwrap()).unwrap();
         }
-        println!("gen {}", path.to_str().unwrap());
+        print!("{} ...", f.to_str().unwrap());
 
-        let mut html = String::new();
-        html::push_html(&mut html, parser);
+        let mut out = String::new();
+
+        if ext.is_none() {
+            panic!("no extension")
+        }
+        let ext = ext.unwrap().to_str().unwrap();
+        let path = match ext {
+            "md" => {
+                let mut options = Options::empty();
+                options.insert(Options::ENABLE_STRIKETHROUGH);
+                options.insert(Options::ENABLE_FOOTNOTES);
+
+                let parser = Parser::new_ext(&src, options);
+                html::push_html(&mut out, parser);
+                path.with_extension("html")
+            }
+            "wd" => {
+                let html = writedown_html::from_str(&src).unwrap();
+                out = html.render();
+                path.with_extension("html")
+            }
+            _ => {
+                println!("[skip]");
+                continue;
+            }
+        };
+
+        // generate HTML header
+        if path.extension().unwrap().to_str().unwrap() == "html" {
+            out = if out.starts_with("<!DOCTYPE>") || out.starts_with("<html>") {
+                out
+            } else {
+                let title = if out.starts_with("<h1>") {
+                    let body = out.strip_prefix("<h1>").unwrap();
+                    let end = body.find("</h1>").unwrap();
+                    &body[..end]
+                } else {
+                    "default title"
+                };
+                let title = if title.len() == 0 {
+                    "".to_string()
+                } else {
+                    format!("  <title>{}</title>\n", title)
+                };
+
+                let head0 = concat!(
+                    "<!DOCTYPE html>\n",
+                    "<html>\n",
+                    "<head>\n",
+                    "  <meta charset=\"UTF-8\">\n",
+                    "  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n"
+                );
+                let head1 = concat!("</head>\n");
+                let header = format!("{}{}{}", head0, title, head1);
+                let footer = concat!("\n</html>");
+
+                format!("{}<body>\n{}\n</body>{}", header, out, footer)
+            };
+        }
+
+        if out.len() == 0 {
+            println!("empty");
+            continue;
+        }
 
         let file = fs::File::create(path)?;
         let mut file = io::BufWriter::new(file);
-        file.write_all(html.as_bytes()).unwrap();
-
-        println!("{}", html);
+        file.write_all(out.as_bytes()).unwrap();
+        println!("[ok]");
     }
 
     Ok(())
